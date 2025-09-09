33dcf87921d177ba4ee428c0741715e8e1f7842c
VMethone <yixiang.vic@gmail.com>
2025-09-09 11:59:55 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,8 +6,12 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
+        dofizzbuzz();
+    }
 
-        for (int i = 1; i < 100; i++) {
+    private static void dofizzbuzz() {
+        int i = 1;
+        while(i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +35,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
